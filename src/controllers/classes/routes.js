5aab7b213e3f3a6585d604e481a757bd31ecf0ad
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2022-03-12 09:00:24 +0700

    update class

--- src/controllers/classes/routes.js
+++ src/controllers/classes/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const createClass = require("./create.classController");
+const updateClass = require("./update.classController");
 
 router.post("/", createClass.service);
+router.put("/:id", updateClass.service);
 
 module.exports = router;
